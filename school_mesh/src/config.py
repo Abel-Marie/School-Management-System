341ff167f74542051365c8662610d646dcfe58bc
Abel Marie <abelmarie49@gmail.com>
2025-12-02 19:37:47 +0300

    Cleared everything to start from scratch again

--- school_mesh/src/config.py
+++
@@ -1,9 +0,0 @@
-from google.genai import types
-
-def get_retry_config():
-    return types.HttpRetryOptions(
-        attempts=5,
-        exp_base=7,
-        initial_delay=1,
-        http_status_codes=[429, 500, 503, 504]
-    )
